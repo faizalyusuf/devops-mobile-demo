2d0d09a73e16b378d97d8a922fe463f0b13ebe21
faizal <faizalace84@gmail.com>
2019-01-04 17:58:02 +0800

    added dockerce install file

---
+++ install_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
