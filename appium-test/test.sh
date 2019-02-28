1057ff9e0f31e9454315cb3545c406a7ca27406b
Dipin Thomas <dipinthomas2003@gmail.com>
2019-02-28 15:41:36 +0800

    Update test.sh
    Adding Uninstall option

--- appium-test/test.sh
+++ appium-test/test.sh
@@ -2,5 +2,6 @@
 
 #$ANDROID_HOME/platform-tools/adb uninstall com.hitherejoe.animate
 cd ../
+$ANDROID_HOME/platform-tools/adb uninstall app/build/outputs/apk/app-debug.apk
 $ANDROID_HOME/platform-tools/adb install app/build/outputs/apk/app-debug.apk
 appium &
